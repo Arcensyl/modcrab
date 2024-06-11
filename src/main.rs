18c931c5fc1f57f43f1df7f181c41eac63e96535
Arcensyn <arcanephys@proton.me>
2024-06-11 04:24:56 -0500

    Did basic project set-up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+//! Modcrab is a programmable mod manager for Linux.
+//! It focuses on Bethesda games, but it can also handle many other games.
+
+mod prelude;
+
+use simple_logger::SimpleLogger;
+use log::LevelFilter;
+
+use crate::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+	SimpleLogger::new()
+		.with_colors(true)
+		.with_level(LevelFilter::Info)
+		.init()
+		.expect("Failed to start logger!");
+
+	info!("Hello from Modcrab!");
 }
---
+++ src/prelude.rs
@@ -0,0 +1,14 @@
+//! This module re-exports a bunch of utilities used across this program.
+
+#![allow(unused_imports)]
+
+pub use mlua::prelude::*;
+pub use tap::prelude::*;
+
+pub use itertools::Itertools;
+pub use thiserror::Error;
+
+pub use log::info;
+pub use log::warn;
+pub use log::error;
+pub use log::debug;
